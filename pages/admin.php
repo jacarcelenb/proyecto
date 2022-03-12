dd0217a41a2971cd5d7f3b8da36da2a2b81e8bae
jacarcelenb <carcelenjorge17@gmail.com>
2022-03-12 16:11:33 -0500

    Añadir includes en las paginas

--- pages/admin.php
+++ pages/admin.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head> 
+<head> 
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -8,18 +8,18 @@
 		
 		<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic|Playfair+Display:400,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
 		<!-- Bootstrap -->
-		<link href="css/bootstrap.min.css" rel="stylesheet">
-		<link href="css/font-awesome.min.css" rel="stylesheet">
+		<link href="../css/bootstrap.min.css" rel="stylesheet">
+		<link href="../css/font-awesome.min.css" rel="stylesheet">
 
-		<link href="css/owl.carousel.css" rel="stylesheet">
-		<link href="css/owl.theme.css" rel="stylesheet">
-		<link href="css/owl.transitions.css" rel="stylesheet">
-		<link href="css/cs-select.css" rel="stylesheet">
-		<link href="css/bootstrap-datepicker3.min.css" rel="stylesheet">
-		<link href="css/freepik.hotels.css" rel="stylesheet">
-		<link href="css/nivo-lightbox.css" rel="stylesheet">
-		<link href="css/nivo-lightbox-theme.css" rel="stylesheet">
-		<link href="css/style.css" rel="stylesheet">
+		<link href="../css/owl.carousel.css" rel="stylesheet">
+		<link href="../css/owl.theme.css" rel="stylesheet">
+		<link href="../css/owl.transitions.css" rel="stylesheet">
+		<link href="../css/cs-select.css" rel="stylesheet">
+		<link href="../css/bootstrap-datepicker3.min.css" rel="stylesheet">
+		<link href="../css/freepik.hotels.css" rel="stylesheet">
+		<link href="../css/nivo-lightbox.css" rel="stylesheet">
+		<link href="../css/nivo-lightbox-theme.css" rel="stylesheet">
+		<link href="../css/style.css" rel="stylesheet">
 
 
 
@@ -31,7 +31,7 @@
 		<script src="js/respond.min.js"></script>
 		<![endif]-->
 
-		<script src="js/modernizr.custom.min.js"></script>
+		<script src="../js/modernizr.custom.min.js"></script>
 	</head>
 	<body>
 		<div class="preloader"></div>
@@ -64,7 +64,7 @@
                     
                         </li> 
 
-                        <li ><a href="index.php">Cerrar Sesion</a>
+                        <li ><a href="../index.php">Cerrar Sesion</a>
                     
                         </li> 
 							
@@ -89,21 +89,4 @@
 
 <!--Colocar el footer-->
 
-		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-		<script src="js/jquery.min.js"></script>
-		<!-- Include all compiled plugins (below), or include individual files as needed -->
-		<script src="js/bootstrap.min.js"></script>
-		<script src="js/owl.carousel.min.js"></script>
-		<script src="js/jssor.slider.mini.js"></script>
-		<script src="js/classie.js"></script>
-		<script src="js/selectFx.js"></script>
-		<script src="js/bootstrap-datepicker.min.js"></script>
-		<script src="js/starrr.min.js"></script>
-		<script src="js/nivo-lightbox.min.js"></script>
-		<script src="js/jquery.shuffle.min.js"></script>
-		<script src="http://maps.google.com/maps/api/js?sensor=true"></script>
-		<script src="js/gmaps.min.js"></script>
-		<script src="js/jquery.parallax-1.1.3.js"></script>
-		<script src="js/script.js"></script>
-	</body>
-</html>
+<?php include('../html_components/footer.php'); ?>
