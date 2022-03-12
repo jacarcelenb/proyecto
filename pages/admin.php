c765dbfde61d44e51ef31fa6e4bc6af7b46fff95
jacarcelenb <carcelenjorge17@gmail.com>
2022-03-12 17:16:13 -0500

    listar quejas en  la vista

--- pages/admin.php
+++ pages/admin.php
@@ -51,7 +51,7 @@
 					<!-- Collect the nav links, forms, and other content for toggling -->
 					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 						<ul class="nav navbar-nav navbar-right">
-							<li ><a href="#">Quejas</a>
+							<li ><a href="listarquejas.php">Quejas</a>
 			
 								
 						</li> 
