995b48a2f211ccbadd3ee5e590edfc1d782af3ff
jacarcelenb <carcelenjorge17@gmail.com>
2022-03-13 18:18:29 -0500

    Modificacion de mensajes de confirmacion

--- pages/listarquejas.php
+++ pages/listarquejas.php
@@ -81,7 +81,7 @@
 <div class="container">
 <?php if (isset($_SESSION['message'])) { ?>
       <div class="alert alert-<?= $_SESSION['message_type']?> alert-dismissible fade show" role="alert">
-        <?= $_SESSION['message']?>
+	  <strong>  <?= $_SESSION['message']?> </strong> 
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
