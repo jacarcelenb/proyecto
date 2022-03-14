8f2ed92a7fb5e3bf12291cbf34ac57d426e7f10f
jacarcelenb <carcelenjorge17@gmail.com>
2022-03-14 16:06:41 -0500

    Conexion bdd en el cloud

--- dbcontroller/database.php
+++ dbcontroller/database.php
@@ -2,10 +2,10 @@
 session_start();
 
 $conn = mysqli_connect(
-  'localhost',
-  'root',
-  '',
-  'fundacion_proyecto'
+  'bi4aidywvbmc2ikj4a4v-mysql.services.clever-cloud.com',
+  'ueu6ycjqjid4dsju',
+  'zaB48y9qrUVW8VRWEdXF',
+  'bi4aidywvbmc2ikj4a4v'
 ) or die(mysqli_erro($mysqli));
 
 ?>
