8f2158eb6c175afdd02bd36a7748205c49c9fa3f
jacarcelenb <carcelenjorge17@gmail.com>
2022-03-15 05:28:57 -0500

    cambios en la oonexion

--- dbcontroller/database.php
+++ dbcontroller/database.php
@@ -2,10 +2,10 @@
 session_start();
 
 $conn = mysqli_connect(
-  'bi4aidywvbmc2ikj4a4v-mysql.services.clever-cloud.com',
-  'ueu6ycjqjid4dsju',
-  'zaB48y9qrUVW8VRWEdXF',
-  'bi4aidywvbmc2ikj4a4v'
+  'localhost',
+  'root',
+  '',
+  'fundacion_proyecto'
 ) or die(mysqli_erro($mysqli));
 
 ?>
