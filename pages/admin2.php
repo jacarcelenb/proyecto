a8d3cde12357c995b3bdb262a20fdd4c11ea53bf
jacarcelenb <carcelenjorge17@gmail.com>
2022-03-15 21:35:04 -0500

    Cambios en la cuenta del admin

--- pages/admin2.php
+++ pages/admin2.php
@@ -100,38 +100,7 @@
 					<center><img src="../images/inscripcion.png" alt="" class="img-responsive">
 					<a href="listarinscripciones.php" class="btn btn-success">Inscripciones</a>
 				</center>
-						
-					</div>
-					<div class="col-md-4">  
-						<center><img src="../images/ICONO-NOTICIAS.jpg" alt="" class="img-responsive">
-						<a href="listarnoticias.php" class="btn btn-success">Noticias</a>
-					</center>
-						
-					</div>
-					<div class="col-md-4">  
-					<center><img src="../images/video1.png" alt="" class="img-responsive">
-					<a href="listarvideos.php" class="btn btn-success">Videos</a>
-				</center>
-					</div>
-					<div class="col-md-4">  
-					<center><img src="../images/fotos.png" alt="" class="img-responsive">
-					<a href="listarfotos.php" class="btn btn-success">Fotos</a>
-				</center>
-					</div>
-					<div class="col-md-4">  
-					<center><img src="../images/actividades.png" alt="" class="img-responsive">
-					<a href="listaractividades.php"class="btn btn-success">Actividades</a>
-					<center>
-					</div>
-					<div class="col-md-4">  
-					<center><img src="../images/blog.png" alt="" class="img-responsive">
-					<a href="listarblogs.php"class="btn btn-success">Blogs</a>
-					<center>
-					
-					</div>
-					
-
-										
+											
 										
 				</div>
 			</div>
--- pages/admin3.php
+++ pages/admin3.php
@@ -84,25 +84,6 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-md-4">  
-					<center><img src="../images/proyectos.png" alt="" class="img-responsive"/>
-					<a href="listarproyectos.php" class="btn btn-success">Proyectos</a>
-				
-				</center>
-						
-					</div>
-					<div class="col-md-4">  
-					<center><img src="../images/quejas.png" alt="" class="img-responsive">
-					<a href="listarquejas.php"  class="btn btn-success">Quejas</a>
-				    </center>
-						
-					</div>
-					<div class="col-md-4">  
-					<center><img src="../images/inscripcion.png" alt="" class="img-responsive">
-					<a href="listarinscripciones.php" class="btn btn-success">Inscripciones</a>
-				</center>
-						
-					</div>
-					<div class="col-md-4">  
 						<center><img src="../images/ICONO-NOTICIAS.jpg" alt="" class="img-responsive">
 						<a href="listarnoticias.php" class="btn btn-success">Noticias</a>
 					</center>
