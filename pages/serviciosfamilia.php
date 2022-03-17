b0e07fa0959108d22c69267f443e14f8c0e04b76
jacarcelenb <carcelenjorge17@gmail.com>
2022-03-17 06:12:45 -0500

    Campos de la navbar

---
+++ pages/serviciosfamilia.php
@@ -0,0 +1,103 @@
+
+<?php include('../html_components/header.php'); ?>
+		<div class="mg-page-title parallax">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<h2>EDUCACIÓN</h2>
+						<p> .</p>
+					</div>
+				</div>
+			</div>
+		</div>
+<!--    INICIO CONTENIDO     --><div class="mg-blog-list">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4">
+						<div class="mg-widget-area">
+							<aside class="mg-widget">
+								<h2 class="mg-widget-title">Areas de Trabajo</h2>
+								<ul class="mg-recnt-posts">
+									<li>
+										<div class="mg-recnt-post">
+											<div class="mg-rp-date"><i class="fa fa-cogs"></i></div>
+											<h3><a href="salud.php">Cuidado de la Salud</a></h3>
+											<p>ASAMBLEA IMBABURA RENACIENTE,  es una organización que tiene como...</p>
+										</div>
+									</li>
+									<li>
+										<div class="mg-recnt-post">
+											<div class="mg-rp-date"><i class="fa fa-cogs"></i></div>
+											<h3><a href="educacion.php">Educación </a></h3>
+											<p>Restaurar El Estado Plurinacional De Ecuador...</p>
+										</div>
+									</li>
+									<li>
+										<div class="mg-recnt-post">
+											<div class="mg-rp-date"><i class="fa fa-cogs"></i></div>
+											<h3><a href="comunicacion.php">Medios de Comunicación</a></h3>
+											<p>Atención médica diseñada para enseñar a las...</p>
+										</div>
+									</li>
+									<li>
+										<div class="mg-recnt-post">
+											<div class="mg-rp-date"><i class="fa fa-cogs"></i></div>
+											<h3><a href="finanza.php">Sistema Financiero</a></h3>
+											<p>El sistema finaciero global ha sido monopolizado...</p>
+										</div>
+									</li>
+									<li>
+										<div class="mg-recnt-post">
+											<div class="mg-rp-date"><i class="fa fa-cogs"></i></div>
+											<h3><a href="web.php">Plataforma Web</a></h3>
+											<p>G-FORCE NETWORK es una plataforma de redes...</p>
+										</div>
+									</li>
+									<li>
+										<div class="mg-recnt-post">
+											<div class="mg-rp-date"><i class="fa fa-cogs"></i></div>
+											<h3><a href="gia.php">GIA</a></h3>
+											<p>La Agencia de Inteligencia Global fue...</p>
+										</div>
+									</li>
+									<li>
+										<div class="mg-recnt-post">
+											<div class="mg-rp-date"><i class="fa fa-cogs"></i></div>
+											<h3><a href="alimentos.php">Alimentos</a></h3>
+											<p>El objetivo de ASAMBLEA IMBABURA RENACIENTE, </p>
+										</div>
+									</li>
+									<li>
+										<div class="mg-recnt-post">
+											<div class="mg-rp-date"><i class="fa fa-cogs"></i></div>
+											<h3><a href="asambleas.php">Asambleas</a></h3>
+											<p>La Asamblea cumple la función de Organizar...</p>
+										</div>
+									</li>
+								</ul>
+							</aside>
+						</div>
+					</div>
+					
+					
+					<div class="col-md-8">
+						<main>
+							<article class="mg-post">
+								<header>
+									<a href="#"><img src="images/educacion.jpg" alt="" class="img-responsive"></a>
+									<h2 class="mg-post-title">Educación</h2>
+								</header>
+								<div>
+									
+
+								<p><strong>Nosotros, como seres humanos en este planeta,</strong>  tenemos la responsabilidad de formar la próxima  generación. Este es un proceso que comienza en casa y  que debe reflejarse en el aula.</p>
+					
+								</div>
+							</article>
+						</main>
+					</div>
+				</div>
+			</div>
+		</div>
+<BR><BR>        
+<?php include('../html_components/footer.php'); ?>
