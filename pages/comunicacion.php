1c0b92c4a103fbb13dbd24af540b7344eb13bd51
jacarcelenb <carcelenjorge17@gmail.com>
2022-03-18 06:22:39 -0500

    correccion de observaciones en el index

--- pages/comunicacion.php
+++ pages/comunicacion.php
@@ -16,67 +16,7 @@
 				<div class="row">
 					<div class="col-md-4">
 						<div class="mg-widget-area">
-							<aside class="mg-widget">
-								<h2 class="mg-widget-title">Areas de Trabajo</h2>
-								<ul class="mg-recnt-posts">
-									<li>
-										<div class="mg-recnt-post">
-											<div class="mg-rp-date"><i class="fa fa-cogs"></i></div>
-											<h3><a href="salud.php">Cuidado de la Salud</a></h3>
-											<p>ASAMBLEA IMBABURA RENACIENTE,  es una organización que tiene como...</p>
-										</div>
-									</li>
-									<li>
-										<div class="mg-recnt-post">
-											<div class="mg-rp-date"><i class="fa fa-cogs"></i></div>
-											<h3><a href="educacion.php">Educación </a></h3>
-											<p>Restaurar El Estado Plurinacional De Ecuador...</p>
-										</div>
-									</li>
-									<li>
-										<div class="mg-recnt-post">
-											<div class="mg-rp-date"><i class="fa fa-cogs"></i></div>
-											<h3><a href="comunicacion.php">Medios de Comunicación</a></h3>
-											<p>Atención médica diseñada para enseñar a las...</p>
-										</div>
-									</li>
-									<li>
-										<div class="mg-recnt-post">
-											<div class="mg-rp-date"><i class="fa fa-cogs"></i></div>
-											<h3><a href="finanza.php">Sistema Financiero</a></h3>
-											<p>El sistema finaciero global ha sido monopolizado...</p>
-										</div>
-									</li>
-									<li>
-										<div class="mg-recnt-post">
-											<div class="mg-rp-date"><i class="fa fa-cogs"></i></div>
-											<h3><a href="web.php">Plataforma Web</a></h3>
-											<p>G-FORCE NETWORK es una plataforma de redes...</p>
-										</div>
-									</li>
-									<li>
-										<div class="mg-recnt-post">
-											<div class="mg-rp-date"><i class="fa fa-cogs"></i></div>
-											<h3><a href="gia.php">GIA</a></h3>
-											<p>La Agencia de Inteligencia Global fue...</p>
-										</div>
-									</li>
-									<li>
-										<div class="mg-recnt-post">
-											<div class="mg-rp-date"><i class="fa fa-cogs"></i></div>
-											<h3><a href="alimentos.php">Alimentos</a></h3>
-											<p>El objetivo de ASAMBLEA IMBABURA RENACIENTE, </p>
-										</div>
-									</li>
-									<li>
-										<div class="mg-recnt-post">
-											<div class="mg-rp-date"><i class="fa fa-cogs"></i></div>
-											<h3><a href="asambleas.php">Asambleas</a></h3>
-											<p>La Asamblea cumple la función de Organizar...</p>
-										</div>
-									</li>
-								</ul>
-							</aside>
+						<?php include("../html_components/workarea.php");?>
 						</div>
 					</div>
 					
