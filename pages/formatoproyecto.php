e84baa392aadc5c30312de7565cfe566a51e710f
jacarcelenb <carcelenjorge17@gmail.com>
2022-03-17 19:27:51 -0500

    Pestaña de presentacion de proyectos

---
+++ pages/formatoproyecto.php
@@ -0,0 +1,29 @@
+<?php include('../html_components/header.php'); ?>
+
+		<div class="mg-page-title parallax">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<h2>Formato de proyectos</h2>
+						<p></p>
+					</div>
+				</div>
+			</div>
+		</div>
+<!--    INICIO CONTENIDO     -->		<div class="mg-page">
+			<div class="container">
+				<div class="row">
+					
+					<div class="col-md-12">
+						<iframe src="../recursos/Formulario de presentación de proyectos.pdf" marginwidth="0" marginheight="0" name="ventana_iframe" scrolling="no" border="0" 
+frameborder="0" width="900" height="900">
+						</iframe> 
+					</div>
+                    
+                    
+					
+				</div>
+			</div>
+		</div>
+<BR><BR>        
+<?php include('../html_components/footer.php'); ?>
