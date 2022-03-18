2a47677105f2014aebb72c1c5914ebabba9cbf5d
jacarcelenb <carcelenjorge17@gmail.com>
2022-03-18 10:25:28 -0500

    cambios en el frontend

--- pages/contacto.php
+++ pages/contacto.php
@@ -12,18 +12,7 @@ include('../html_components/header.php'); ?>
 					</div>
 				</div>
 			</div>
-		</div>
-		<div class="container">
-<?php if (isset($_SESSION['message'])) { ?>
-      <div class="alert alert-<?= $_SESSION['message_type']?> alert-dismissible fade show" role="alert">
-	  <strong>  <?= $_SESSION['message']?> </strong> 
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <?php session_unset(); } ?>
-      
-	<div>
+			</div>
 				<div class="mg-page">
 			<div class="container">
 				<div class="row">
