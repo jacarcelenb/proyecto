b65a9eb3257926e0871a8ef4222b8a9a7844e7a2
jacarcelenb <carcelenjorge17@gmail.com>
2022-03-20 15:57:08 -0500

    Creacion de mensaje de correo

---
+++ html_components/correoinvalido.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../css/errorcorreo.css">
+  </head>
+  <body>
+    <div class="center">
+      <h1>No se puede enviar el correo</h1>
+        <div class="signup_link">
+           <a href="contacto.php">Regresar</a>
+        </div>
+     
+    </div>
+
+  </body>
+</html>
---
+++ html_components/correovalido.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../css/correo.css">
+  </head>
+  <body>
+    <div class="center">
+      <h1>Correo enviado correctamente</h1>
+        <div class="signup_link">
+           <a href="contacto.php">Regresar</a>
+        </div>
+     
+    </div>
+
+  </body>
+</html>
